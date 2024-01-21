076913e4556cce93feee9275058c2d2ef2deab32
Visibyte <nathanglass26@gmail.com>
2024-01-21 11:52:30 +0000

    start customize form card

---
+++ src/components/customize-form.tsx
@@ -0,0 +1,19 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+
+function CustomizeForm() {
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Personal Details</CardTitle>
+          <CardDescription>Let potential employers know a little bit about you.</CardDescription>
+              </CardHeader>
+              <CardContent>
+                  form...
+              </CardContent>
+      </Card>
+    </>
+  );
+}
+
+export default CustomizeForm;
